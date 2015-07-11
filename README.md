02901a9149d24ec9b6ebf718c5eb4a33548f50d7
julianpeeters <julianpeeters@gmail.com>
2015-07-11 00:43:56 -0700

    release versoin 0.0.2 with support for avro AVDL files

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Add the plugin according to the `sbt documentation`_.
 For instance, add the following lines to the file ``hello/project/build.sbt`` in your
 project directory:
 
-    addSbtPlugin("com.julianpeeters" % "sbt-avrohugger" % "0.0.1")
+    addSbtPlugin("com.julianpeeters" % "sbt-avrohugger" % "0.0.2")
 
 
 Import the plugin settings
@@ -41,7 +41,7 @@ Settings
 
 | Name          | Name in shell | Default  | Description  |
 | ------------- |:-------------:| -----:| -----:|
-| sourceDirectory     | ``source-directory`` | ``src/main/avro`` | Path containing ``*.avsc`` and ``*.avro`` files. |
+| sourceDirectory     | ``source-directory`` | ``src/main/avro`` | Path containing ``*.avsc``, ``*.avdl``, and/or ``*.avro`` files. |
 | scalaSource      | ``scala-source``      |   ``$sourceManaged/compiled_avro`` |   Path for the generated ``*.scala`` files. |
 
 
--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,13 @@ object build extends Build {
         base = file("."),
         settings = Defaults.defaultSettings ++ scriptedSettings ++ Seq[Project.Setting[_]](
             organization := "com.julianpeeters",
-            version := "0.0.1",
+            version := "0.0.2",
             sbtPlugin := true,
+            scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ywarn-value-discard"),
             resolvers += Resolver.file("Local Ivy Repository", file("/home/julianpeeters/.ivy2/local/"))(Resolver.ivyStylePatterns),
             libraryDependencies ++= Seq(
-                "com.julianpeeters" % "avrohugger-core_2.10" % "0.0.2",
-                 "org.apache.avro" % "avro" % "1.7.4"),
+                "com.julianpeeters" % "avrohugger-core_2.10" % "0.1.0",
+                "org.apache.avro" % "avro" % "1.7.4"),
             scalaVersion := "2.10.4",
             scalacOptions in Compile ++= Seq("-deprecation"),
             description := "Sbt plugin for compiling Avro sources",
