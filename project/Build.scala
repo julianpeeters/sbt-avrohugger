9919e8288f9a0cfdc2b6fae74380f36c98c92ceb
julianpeeters <julianpeeters@gmail.com>
2015-08-10 03:38:04 -0700

    update avrohugger and release 0.2.3

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,12 @@ object build extends Build {
     base = file("."),
     settings = Defaults.defaultSettings ++ scriptedSettings ++ Seq[Project.Setting[_]](
       organization := "com.julianpeeters",
-      version := "0.2.2",
+      version := "0.2.3",
       sbtPlugin := true,
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ywarn-value-discard"),
       resolvers += Resolver.file("Local Ivy Repository", file("/home/julianpeeters/.ivy2/local/"))(Resolver.ivyStylePatterns),
       libraryDependencies ++= Seq(
-        "com.julianpeeters" % "avrohugger-core_2.10" % "0.4.0"),
+        "com.julianpeeters" % "avrohugger-core_2.10" % "0.5.0"),
       scalaVersion := "2.10.5",
       scalacOptions in Compile ++= Seq("-deprecation"),
       description := "Sbt plugin for compiling Avro sources",
