fe564f7b910bd8c903df28996b688999778293f8
Julian Peeters <julianpeeters@gmail.com>
2017-12-31 20:45:32 -0800

    Restore Standard format default value tests

--- src/sbt-test/avrohugger/GenericJavaEnumSerializationTests/src/test/scala/test/standard/StandardDefaultValuesSpec.scala
+++ src/sbt-test/avrohugger/GenericJavaEnumSerializationTests/src/test/scala/test/standard/StandardDefaultValuesSpec.scala
@@ -1,35 +1,34 @@
 import test._
 import org.specs2.mutable.Specification
 import com.sksamuel.avro4s.RecordFormat
+import java.io.File
 
+import org.apache.avro.file.{ DataFileReader, DataFileWriter }
+import org.apache.avro.generic.{
+  GenericRecord,
+  GenericDatumReader,
+  GenericDatumWriter}
+  
 class StandardDefaultValuesSpec extends Specification {
-  /*
-  Avro4s is used to convert to `GenericRecord` for testing, chokes on `map`
+
   "A case class with default values" should {
     "deserialize correctly" in {
     
+      val format = RecordFormat[DefaultTest]
       val record = DefaultTest()
-      val records = List(format.to(record))
-      
-      val fileName = s"${records.head.getClass.getName}"
-      val fileEnding = "avro"
-      val file = File.createTempFile(fileName, fileEnding)
-      file.deleteOnExit()
-      SpecificTestUtil.write(file, records)
-      
-      val sameRecord = reader.read(filename).head
-      
-      sameRecord.suit === SPADES
+      val avro = format.to(record)
+      val sameRecord = format.from(avro)
+      sameRecord.suit === DefaultEnum.SPADES
       sameRecord.number === 0
       sameRecord.str === "str"
       sameRecord.optionString === None
       sameRecord.optionStringValue === Some("default")
       sameRecord.embedded === Embedded(1)
-      sameRecord.defaultArray === Array(1,3,4,5)
+      sameRecord.defaultArray === List(1,3,4,5)
       sameRecord.optionalEnum === None
       sameRecord.defaultMap === Map("Hello" -> "world", "Merry" -> "Christmas")
       sameRecord.byt === "\u00FF".getBytes
     }
   }
-  */
+
 }
